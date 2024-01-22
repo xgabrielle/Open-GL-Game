9860131a649dd4bd049ee1bc11ab668ec9f8f414
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-01-22 15:56:37 +0100

    Hello window

--- Open-GL-Game/Open-GL-Game.cpp
+++ Open-GL-Game/Open-GL-Game.cpp
@@ -1,9 +1,56 @@
 
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+using namespace std;
+
+void error_callback(int error, const char* msg) {
+    cerr << " [" << error << "] " << msg << endl;
 }
 
+int main() 
+{
 
+    glfwSetErrorCallback(error_callback);
+
+    // Initialize GLFW
+    if (!glfwInit()) { // Exit, if it failed
+        cout << "Failed to init GLFW" << endl;
+        return -1;
+    }
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    //Requesting Window from Operating System
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == nullptr)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // Initialize GLAD (connects OpenGL Functions)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+
+    //While the User doesn't want to Quit (X Button,Alt+Fn)
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    // Cleans up alla the GLFW stuff
+
+    glfwTerminate();
+    return 0;
+}
